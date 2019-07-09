6fd7c1330997524bd9ebee4adb9c4f677d8e99ff
EscapeOrNot <escapeornot75@gmail.com>
2019-07-09 19:10:45 +0200

    Add login interface

--- os-app-react/src/App.jsx
+++ os-app-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Modal, Progress, Spin } from 'antd'
+import { Icon, Input, message, Modal, Progress, Spin } from 'antd'
 
 import './App.less'
 
@@ -18,12 +18,22 @@ class App extends Component {
       countDownRunning: false,
       folderOpen: false,
       folderOpenName: '',
+	    loggedIn: false,
     }
 
     this.handleCancel = this.handleCancel.bind(this)
     this.handleOk = this.handleOk.bind(this)
     this.showModal = this.showModal.bind(this)
     this.startCountDown = this.startCountDown.bind(this)
+    this.checkPwd = this.checkPwd.bind(this)
+  }
+  
+  checkPwd(value) {
+    if (value === 'moscovium289') {
+      this.setState({ loggedIn: true });
+    } else {
+      message.error('Mot de passe invalide.')
+    }
   }
 
   handleOk() {
@@ -67,72 +77,89 @@ class App extends Component {
       countDownRunning,
       folderOpen,
       folderOpenName,
+	    loggedIn,
     } = this.state
 
     return (
       <div className="App">
-        <div className="folder" onClick={() => this.showModal('Project 1')}>
-          <div className="folder-name">DNA Sequencing</div>
-        </div>
-        <GDModal />
-        <div className="folder" onClick={() => this.showModal('Project 3')}>
-          <div className="folder-name">Classical Genetic Research</div>
-        </div>
-        <div className="folder" onClick={() => this.showModal('Project 4')}>
-          <div className="folder-name">Genetic Characterization</div>
-        </div>
-        <div id="copyToUSBButtonContainer">
-          {!countDownRunning ? (
-            <div id="copyToUSBButton" onClick={this.startCountDown}>
-              Copy to USB
+		{loggedIn ?
+			<Fragment>
+				<div className="folder" onClick={() => this.showModal('Project 1')}>
+		          <div className="folder-name">DNA Sequencing</div>
+		        </div>
+		        <GDModal />
+		        <div className="folder" onClick={() => this.showModal('Project 3')}>
+		          <div className="folder-name">Classical Genetic Research</div>
+		        </div>
+		        <div className="folder" onClick={() => this.showModal('Project 4')}>
+		          <div className="folder-name">Genetic Characterization</div>
+		        </div>
+		        <div id="copyToUSBButtonContainer">
+		          {!countDownRunning ? (
+		            <div id="copyToUSBButton" onClick={this.startCountDown}>
+		              Copy to USB
+		            </div>
+		          ) : (
+		            <div className="copy-panel">
+		              <b>
+		                {copyingPercent < 100 ? (
+		                  <Fragment>
+		                    <Spin type="info" />
+		                    &nbsp;Copying data to USB, do not unplug <small>({copyingPercent}%)</small>
+		                  </Fragment>
+		                ) : (
+		                  'Copy complete!'
+		                )}
+		              </b>
+		              <Progress percent={copyingPercent} status="active" showInfo={false} />
+		            </div>
+		          )}
+		        </div>
+		        <Modal
+		          title={<div>{folderOpenName}</div>}
+		          visible={folderOpen}
+		          onOk={this.handleOk}
+		          onCancel={this.handleCancel}
+		          footer={null}
+		          width={800}
+		        >
+		          <embed
+		            src={
+		              folderOpenName === 'Project 1'
+		                ? process.env.PUBLIC_URL + '/DNA-SEQUENCING.pdf'
+		                : (folderOpenName === 'Project 3' &&
+		                    process.env.PUBLIC_URL + '/CLASSICAL-GENETIC-RESEARCH.pdf') ||
+		                  process.env.PUBLIC_URL + '/GENETIC-CHARACTERIZATION.pdf'
+		            }
+		            width="100%"
+		            height="600"
+		            type="application/pdf"
+		          />
+		        </Modal>
+		        <Modal
+		          closable={copyingPercent >= 100}
+		          footer={null}
+		          maskClosable={copyingPercent >= 100}
+		          onCancel={() =>
+		            copyingPercent >= 100 && this.setState({ copyingModalOpen: false, copyingPercent: 0 })
+		          }
+		          title="Copying to USB"
+		          visible={copyingModalOpen}
+		        />
+			    </Fragment> :
+		      <div className="login-form">
+    		    	<div className="login-label"><Icon type="user" className="login-icon" /> E. Wells</div>
+  			    <div className="login-input">
+    		      <Input.Search
+                type="password"
+    			      placeholder="Mot de passe"
+    			      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                onSearch={value => this.checkPwd(value)}
+    			    />
             </div>
-          ) : (
-            <div className="copy-panel">
-              <b>
-                {copyingPercent < 100 ? (
-                  <Fragment>
-                    <Spin type="info" />
-                    &nbsp;Copying data to USB, do not unplug <small>({copyingPercent}%)</small>
-                  </Fragment>
-                ) : (
-                  'Copy complete!'
-                )}
-              </b>
-              <Progress percent={copyingPercent} status="active" showInfo={false} />
-            </div>
-          )}
-        </div>
-        <Modal
-          title={<div>{folderOpenName}</div>}
-          visible={folderOpen}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-          footer={null}
-          width={800}
-        >
-          <embed
-            src={
-              folderOpenName === 'Project 1'
-                ? process.env.PUBLIC_URL + '/DNA-SEQUENCING.pdf'
-                : (folderOpenName === 'Project 3' &&
-                    process.env.PUBLIC_URL + '/CLASSICAL-GENETIC-RESEARCH.pdf') ||
-                  process.env.PUBLIC_URL + '/GENETIC-CHARACTERIZATION.pdf'
-            }
-            width="100%"
-            height="600"
-            type="application/pdf"
-          />
-        </Modal>
-        <Modal
-          closable={copyingPercent >= 100}
-          footer={null}
-          maskClosable={copyingPercent >= 100}
-          onCancel={() =>
-            copyingPercent >= 100 && this.setState({ copyingModalOpen: false, copyingPercent: 0 })
-          }
-          title="Copying to USB"
-          visible={copyingModalOpen}
-        />
+    		  </div>
+    		}
+        
       </div>
     )
   }
